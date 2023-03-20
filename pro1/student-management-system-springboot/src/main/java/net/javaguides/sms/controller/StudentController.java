6ff9e749cb49651e3b4204d3e0dad7e8836a8104
Sanithi Shavindi <sandayanasanithi@gmail.com>
2023-03-20 11:16:00 +0530

    ....

--- pro1/student-management-system-springboot/src/main/java/net/javaguides/sms/controller/StudentController.java
+++ pro1/student-management-system-springboot/src/main/java/net/javaguides/sms/controller/StudentController.java
@@ -1,23 +1,44 @@
 package net.javaguides.sms.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import net.javaguides.sms.entity.Student;
+import net.javaguides.sms.repository.StudentRepository;
 import net.javaguides.sms.service.StudentService;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.List;
 
 @Controller
+@RestController
+@CrossOrigin("http://localhost:3306")
 public class StudentController {
-	
+	@Autowired 
+	private StudentRepository studentRepository;
 	private StudentService studentService;
+
+	@PostMapping("/students")
+	Student newStudent(@RequestBody Student newStudent){
+		return studentRepository.save(newStudent);
+	}
+	
+	@GetMapping("/students")
+	List<Student> getAllStudents() {
+		return studentRepository.findAll();
+	}
+	
 
 	public StudentController(StudentService studentService) {
 		super();
-		this.studentService = studentService;
+		this.studentRepository = studentRepository;
 	}
 	
 	// handler method to handle list students and return mode and view
@@ -37,11 +58,11 @@ public class StudentController {
 		
 	}
 	
-	@PostMapping("/students")
+	 @PostMapping("/students")
 	public String saveStudent(@ModelAttribute("student") Student student) {
-		studentService.saveStudent(student);
-		return "redirect:/students";
-	}
+	 	studentService.saveStudent(student);
+	 	return "redirect:/students";
+ }
 	
 	@GetMapping("/students/edit/{id}")
 	public String editStudentForm(@PathVariable Long id, Model model) {
@@ -67,9 +88,9 @@ public class StudentController {
 		// save updated student object
 		studentService.updateStudent(existingStudent);
 		return "redirect:/students";		
-	}
+			}
 	
-	// handler method to handle delete student request
+	//handler method to handle delete student request
 	
 	@GetMapping("/students/{id}")
 	public String deleteStudent(@PathVariable Long id) {
